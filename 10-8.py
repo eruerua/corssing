08b130761cf4440632422e0b117c7316ad18ee10
侯浩 <houhiroshi@houhaodeMacBook-Pro.local>
2019-07-28 16:36:18 +0800

    10-8

---
+++ 10-8.py
@@ -0,0 +1,8 @@
+import requests
+city=input("请输入城市名称：")
+key='dkq4nrn0wfjexgjw'
+url='https://api.seniverse.com/v3/weather/now.json?key='+key+'&location='+city+'&language=zh-Hans&unit=c'
+r = requests.get(url)
+message=r.json()
+print(message)
+print('%s今天天气为%s,温度为%s摄氏度'%(message['results'][0]['location']['name'],message['results'][0]['now']['text'],message['results'][0]['now']['temperature']))
